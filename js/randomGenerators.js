f14d098840ddcf32d3cd47b2126429353db8256f
Jana Ma <janama@Janas-MacBook-Pro.local>
2018-10-02 15:20:08 -0700

    jana week 3 code

--- js/randomGenerators.js
+++ js/randomGenerators.js
@@ -2,21 +2,55 @@
 // TODO: Use constructor notation to create an object
 // with properties and methods that can be shared by both
 // diceRollGenerator and eightBallGenerator.
-function Generator() {
-  // YOUR CODE HERE
+function Generator(array) {
+  this.array = array || []
+  this.generate = function (){
+    var randomNumber = Math.random();
+    var randomAnswer = Math.floor(randomNumber * this.array.length);
+    var answer = this.array [randomAnswer];
+    return answer;
+  };
+
+this.addResult = function(value) {
+  this.array.push(value)
+};
+
+this.removeResult = function(value) {
+  for(var i = 0; i < this.array.length-1; i++){
+    if (array[i] === value) {
+      this.array.splice(i,1);
+    }
+  }
 }
 
 // TODO: Initialise diceRollGenerator and eightBallGenerator
 // using the constructor notation and the Generator object
 // you just created
-const diceRollGenerator = null;
-const eightBallGenerator = null;
+const diceRollGenerator{
+  diceRollGenerator.first = 1
+  diceRollGenerator.second = 2
+  diceRollGenerator.third = 3
+  diceRollGenerator.fourth = 4
+  diceRollGenerator.fifth = 5
+  diceRollGenerator.sixth = 6
+}
+const eightBallGenerator = {
+  eightBallGenerator.first = 1
+  eightBallGenerator.second = 2
+  eightBallGenerator.third = 3
+  eightBallGenerator.fourth = 4
+  eightBallGenerator.fixth = 5
+  eightBallGenerator.sixth = 6
+}
+
 
 function handleDiceRoll() {
   const resultElement = document.getElementById('diceResult');
+  var diceResult = math.random()*6;
 
   // TODO: call a method on diceRollGenerator to populate result with a random value
-  const result = "result";
+  var RoundedNumber = math.cell(diceResult)
+;  const result = "result";
   resultElement.innerHTML = result;
 }
 
